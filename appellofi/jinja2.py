33eb897b5d93f9e8e492d0d5945f4ce8c8499268
baleen37 <baleen37@gmail.com>
2017-06-05 21:41:22 +0900

    Add hash

--- appellofi/jinja2.py
+++ appellofi/jinja2.py
@@ -1,13 +1,28 @@
-from django.contrib.staticfiles.storage import staticfiles_storage
+import subprocess
+
+from django.conf import settings
+from django.contrib import messages
 from django.urls import reverse
 
 from jinja2 import Environment
 
 
 def environment(**options):
+    def static_cache_query(url):
+
+        out = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE).communicate()[0]
+
+        try:
+            git_hash = out.strip().decode('ascii')
+        except OSError:
+            git_hash = "f9e9a0"
+
+        return "%s%s?%s" % (settings.STATIC_URL, url, git_hash)
+
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage.url,
+        'static': static_cache_query,
         'url': reverse,
+        'get_messages': messages.get_messages,
     })
     return env
